360ca14fe5c39bedf822ee6bd13823947180bd6a
MaxBetA685 <46646028+MaxBetA685@users.noreply.github.com>
2019-01-13 14:20:23 +0300

    Add files via upload

---
+++ WindowsFormsApp18/WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.closeButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.userTextLabel = new System.Windows.Forms.Label();
+            this.readTextButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(835, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(74, 26);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Stop";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 158);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 84);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlignChanged += new System.EventHandler(this.textBox1_TextAlignChanged);
+            // 
+            // userTextLabel
+            // 
+            this.userTextLabel.AutoSize = true;
+            this.userTextLabel.Location = new System.Drawing.Point(121, 53);
+            this.userTextLabel.Name = "userTextLabel";
+            this.userTextLabel.Size = new System.Drawing.Size(121, 13);
+            this.userTextLabel.TabIndex = 2;
+            this.userTextLabel.Text = "Тут твой комент будет";
+            // 
+            // readTextButton
+            // 
+            this.readTextButton.Location = new System.Drawing.Point(101, 298);
+            this.readTextButton.Name = "readTextButton";
+            this.readTextButton.Size = new System.Drawing.Size(180, 23);
+            this.readTextButton.TabIndex = 3;
+            this.readTextButton.Text = "Нажми что бы оставить комент";
+            this.readTextButton.UseVisualStyleBackColor = true;
+            this.readTextButton.Click += new System.EventHandler(this.readTextButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(385, -12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 466);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(255, 1);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Показать картинку";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.readTextButton);
+            this.Controls.Add(this.userTextLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.closeButton);
+            this.Name = "Form1";
+            this.Text = "заголовок";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label userTextLabel;
+        private System.Windows.Forms.Button readTextButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp18/WindowsFormsApp18/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void readTextButton_Click(object sender, EventArgs e)
+        {
+            userTextLabel.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextAlignChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = checkBox1.Checked;
+        }
+    }
+}
